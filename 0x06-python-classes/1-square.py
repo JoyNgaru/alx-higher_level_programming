b05dad972e0bdeee67ba2d069916a1ae60b85a30
JoyNgaru <joywanjikum@gmail.com>
2023-06-26 22:58:35 -0700

    create a new instance of the  class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define a class Square from task 0."""
+
+
+class Square:
+    """Represent a square and now give it attribute size yk, size of a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
