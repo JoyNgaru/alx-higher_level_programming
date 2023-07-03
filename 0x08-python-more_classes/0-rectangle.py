231df83e43e6dc2ca37e5cb54c2d1a5a367bbaf4
JoyNgaru <joywanjikum@gmail.com>
2023-07-03 05:11:28 -0700

    create class rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Defines class rectangle"""
+"""Defines a rectangle class"""
+
+
 class Rectangle:
-      """Represents a Rectangle"""
+    """Represents a Rectangle"""
     pass
