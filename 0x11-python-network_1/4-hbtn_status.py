1cd6585f22648a293a6d3aca78e4bda1000bb1c0
JoyNgaru <joywanjikum@gmail.com>
2023-09-29 06:19:04 -0700

    fetches https://intranet.hbtn.io/status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 """
-Python script that fetches https://intranet.hbtn.io/status
+ fetches https://alx-intranet.hbtn.io/status
 """
+
 import requests
 
-if __name__ == '__main__':
+url = "https://alx-intranet.hbtn.io/status"
 
-    res = requests.get("https://intranet.hbtn.io/status")
+try:
+    response = requests.get(url)
+    response.raise_for_status()
+
+    content = response.text
+
     print("Body response:")
-    print("\t- type: {}".format(type(res.text)))
-    print("\t- content: {}".format(res.text))
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+
+except requests.exceptions.RequestException as e:
+    print(f"Error fetching URL: {e}")
